d8dbcecc9e94ff99b1d5b4e6b30a5441ad9afe49
Kent Chan <kntchn@gmail.com>
2020-01-03 22:36:23 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,26 @@
 # Your Code Here
+ def map(array)
+   num_elements = array.length
+   num_elements.times do |index|
+     array[index] = yield(array[index])
+   end
+   array
+ end
+
+def reduce(array, starting_point = nil)
+  num_elements = array.length
+
+  if starting_point
+    answer = starting_point
+    i = 0;
+    else
+      answer = array[0]
+      i = 1;
+    end
+
+  while i < num_elements
+  answer =  yield(answer, array[i])
+  i += 1
+  end
+  return answer
+end
