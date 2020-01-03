b73c94899ca0ba78282440bd70fc7109ed487464
Kent Chan <kntchn@gmail.com>
2020-01-03 22:09:15 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,9 +9,9 @@
 
 def reduce(array, starting_point = 0)
   num_elements = array.length
-  total = starting_point
+  answer = starting_point
   num_elements.times do |index|
-  total =  yield(total, array[index])
+  answer =  yield(answer, array[index])
   end
-  total
+  answer
 end
